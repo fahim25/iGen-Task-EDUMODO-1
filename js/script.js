0669ca318edbffeec95651ec49ecd288e0f2f9ef
fahim25 <fahimmuntasir201@gmail.com>
2020-12-26 01:53:09 +0600

    cookies popup

--- js/script.js
+++ js/script.js
@@ -5,3 +5,22 @@ jQuery(function ($) {
         $('.menu').stickUp();
     });
 });
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.add("active");
+  localStorage.setItem("cookieBannerDisplayed", "false");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 0);
